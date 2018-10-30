ca27dbbb1cb85d837f662f9d559d081fff2bfff5
Fernando Lucas Santos <fernando@formahomolog.com.br>
2018-10-30 12:40:18 -0300

    realizado pesquisa de personagens, detalhando informações sobre o personagem escolhido e refatorado codigo

--- js/main.js
+++ js/main.js
@@ -1,54 +1,98 @@
-var URL_API_SWAPI = "https://swapi.co/api/people";
-var page1 = "page=1";
-var personagens = [];
+const URL_API_SWAPI_GET_PEOPLE = "https://swapi.co/api/people";
+const resultadosPorPagina = 10;
+const selectP = document.querySelector("#selectPersonagem");
+const carregando = document.querySelector("#carregando");
+let personagens = [];
+let totalPersonagens;
+let pageNumber = 1;
 
-function pegarInfo() {
-  for (let i = 1; i < 10; i++) {
-    $.getJSON(URL_API_SWAPI + "?page=" + i , function( data ) {
-    })
-    .done(function(data) {
-      data.results.map(item => {
-        personagens.push(item);
-      });
-      
-      if(personagens.length == 87){
-        
-        // console.log(personagens.sort());
-console.log(personagens.sort(function(a, b) { 
-  return a.name - b.name;
-}), "personagens")
-        $.each(personagens, function(val, text) {
-          console.log(val, text)
-          $('#opcoes').append(
-            
-              $('<option></option>').val(text.name).html(text.name)
-          );
-        })
-        // popularSelect(personagens.sort());
-      }    
-    });
-  }
+let arrayApi = [];
+
+function ordenar(itemA, itemB) {
+  if (itemA.name < itemB.name) return -1;
+  if (itemA.name > itemB.name) return 1;
+  return 0;
 }
 
-// function popularSelect(personagens) {
-//   var autors = personagens.sort();
-//   console.log(autors, "autors")
-//   const selectP = document.querySelector('#selectPersonagem');
-//   let html = '<option value="">Carregando...</option>';
+// consome api para buscar personagens
+async function buscarPersonagens(pageNumber) {
+  console.log("aqui", pageNumber);
+  let response = await $.getJSON(
+    URL_API_SWAPI_GET_PEOPLE,
+    {
+      page: pageNumber
+    },
+    function(data) {
+      totalPersonagens = Number(data.count);
+    }
+  );
+  response.results.map(item => {
+    personagens.push(item);
+  });
+  return personagens;
+}
 
-//   // $.each(personagens, function(val, text) {
-//   //   $('#selectPersonagem').append(
-      
-//   //       $('<option></option>').val(val.name).html(text)
-//   //   );
-//   // })
-//   autors.forEach(item => {
-//     html += `
-//       <option value="${item.url}">${item.name}</option>
-//     `;
-//   });
+buscarTodosPersonagens(pageNumber);
 
-//   selectP.innerHTML = html;
-// }
+function buscarTodosPersonagens(pageNumber) {
+  buscarPersonagens(pageNumber)
+    .then(response => {
+      console.log(response, "response");
+      if (pageNumber < totalPersonagens / resultadosPorPagina) {
+        pageNumber++;
+        buscarTodosPersonagens(pageNumber);
+      } else {
+        popularSelect(personagens.sort(ordenar));
+      }
+    })
+    .catch(error => {
+      console.log(error, "error");
+    });
+}
 
-pegarInfo();
+function popularSelect(personagens) {
+  let html = "";
+
+  personagens.forEach(item => {
+    html += `
+      <option value="${item.url}">${item.name}</option>
+    `;
+  });
+
+  selectP.innerHTML = html;
+  carregando.remove();
+}
+
+$("#selectPersonagem").change(function() {
+  let url = $("#selectPersonagem option:selected").val();
+  detalharPersonagem(url)
+    .then(personagemSelecionado => {
+      console.log(personagemSelecionado, "sucesso");
+      $("#infoPersonagem")
+        .find("p")
+        .remove();
+      let html = `
+                  <p>Name: ${personagemSelecionado.name}</p>
+                  <p>Gender: ${personagemSelecionado.gender}</p>
+                  <p>Hair color: ${personagemSelecionado.hair_color}</p>
+                  <p>Height: ${personagemSelecionado.height}</p>
+                  <p>Skin color: ${personagemSelecionado.skin_color}</p>
+                  `;
+
+      $("#infoPersonagem").append($(html));
+    })
+    .catch(error => {
+      console.log(error, "error");
+    });
+  console.log(personagemSelecionado);
+});
+
+async function detalharPersonagem(url) {
+  let infoPersonagem = await $.getJSON(url, function(data) {});
+  return infoPersonagem;
+}
+
+document.addEventListener("mousemove", () => {
+  const audio = document.querySelector("audio");
+  audio.play();
+});
